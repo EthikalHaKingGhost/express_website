26f781bcdea3e859940ff3c0adbbc693dc0aef8e
EthikalHakingGhost <travo.edward@gmail.com>
2023-02-15 22:36:08 -0400

    Added views and layout sample

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-const port = 3000;
+const pug = require("pug");
+
+const PORT = 3000;
 
 const app = express();
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port, ${PORT}`);
+});
+
+//Join Directory name with template paths using views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () => {
-  console.log("Server is running on port ${port}");
-});
-
 app.get("/", function (req, res) {
-  console.log("Hello World!");
-  res.send("Hello World");
+  res.render("index");
 });
